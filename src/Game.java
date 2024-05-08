94c2c8bfd4f52c7b31b1fcec6e4a816266e080b9
Gervig <gervig91@gmail.com>
2024-05-08 12:04:27 +0200

    casper create Game.java

---
+++ src/Game.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Game {
+    private String name;
+    private boolean gameRunning;
+    private boolean mainMenuRunning;
+    private boolean settingsMenuRunning;
+    private boolean leaderboardsMenuRunning;
+    private boolean exitGameMenuRunning;
+    private ArrayList<Integer> highScores;
+
+    // extra feature for later
+//    public AudioPlayer audioPlayer;
+    public Player player;
+    public Obstacle block;
+    public Obstacle speedUp;
+    public PowerUp heal;
+    public PowerUp slowTime;
+    public PowerUp superTime;
+
+    public Game(String name) {
+        this.name = name;
+    }
+
+    public void draw(){
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void runStartMenu(){
+
+    }
+
+    public void settingsMenu(){
+
+    }
+
+    public void runGameChecks(){
+
+    }
+
+    public void continueGame(){
+
+    }
+
+    public void playerDeath(){
+
+    }
+
+    public void quitProgram(){
+        
+    }
+}
