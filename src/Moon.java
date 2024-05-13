3c471b17e9b9d9e1477756be7aa7367c0679c911
Gervig <gervig91@gmail.com>
2024-05-13 10:07:18 +0200

    drawMoon()

--- src/Moon.java
+++ src/Moon.java
@@ -21,5 +21,29 @@ public class Moon {
 
     public void drawMoon() {
         // Implementation here
+
+        // pushmatrix saves the current camera position
+        pApplet.pushMatrix();
+        // make them bounce on the edges by reversing their speed whenever they get out of bounds.
+        if (t + startX > pApplet.width * 1.22 || t + startX < 0) {
+            moonSpeed *= -1;
+        }
+        // move the camera position slightly before drawing each moon, simluating movement
+        pApplet.translate(t + startX, 1200 - moonEDFC, 180 + moonHeight);
+
+        // multiply the moon speed by 5, to compliment the fast forward function for the landscape.
+        if (pApplet.mousePressed) {
+            t += moonSpeed * 5;
+        } else {
+            t += moonSpeed;
+        }
+
+        pApplet.stroke(moonColor);
+        pApplet.fill(moonColor);
+        pApplet.sphere(size);
+        // popmatrix will reset the camera position after the moon is drawn, this avoid messing everything else up with the contantly changing camera
+        // yes, there is no better way to do this in processing. i looked.
+        pApplet.popMatrix();
     }
+}
 }
